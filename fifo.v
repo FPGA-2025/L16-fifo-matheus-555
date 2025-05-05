fe9261faf624f9ca6649b0bb0b8395f80bd791d5
Matheus de Alencar <sletezes@gmail.com>
2025-05-05 15:58:28 -0300

    tentativa 1

--- fifo.v
+++ fifo.v
@@ -8,8 +8,31 @@ module fifo(
     output full,
     output empty
 );
+    reg [7:0] buffer [0:3];
+    
+    reg [1:0] w_ptr, r_ptr;
+    
+    assign empty = (w_ptr == r_ptr);
+    assign full =  (w_ptr == r_ptr-1) || (w_ptr == 3 && r_ptr == 0);
+    
+    // Lógica de escrita
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            w_ptr = 2'b00;
+            r_ptr = 2'b00;
+        end else begin
 
-// insira seu código aqui
+            if (wr_en && !full) begin
+                buffer[w_ptr] = data_in;
+                w_ptr = w_ptr + 1;
+            end
+            
+            if (rd_en && !empty) begin
+                data_out = buffer[r_ptr];
+                r_ptr = r_ptr + 1;
+            end
+            
+        end
+    end
 
 endmodule
-
